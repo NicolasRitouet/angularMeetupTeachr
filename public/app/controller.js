5939922be26dfa98d40d06b8125b9b8dda7eda76
Nicolas Ritouet <nicolas.ritouet@zanox.com>
2014-01-31 15:30:29 +0100

    Add login logic

--- public/app/controller.js
+++ public/app/controller.js
@@ -1,13 +1,10 @@
 'use strict';
 
 angular.module('angularmeetupteachrApp')
-.controller('MainCtrl', function ($scope, $http, $log) {
+.controller('MainCtrl', function ($scope, $http, $log, $location) {
     
 
-    $scope.isAuthenticated = false;
-    $scope.currentUser = {
-        username: 'nicolas@ritouet.com'
-    }
+    $scope.currentUser = {}
     // Load the classrooms
     $http.get('/classrooms').success(function(data) {
         $scope.classrooms = data;
@@ -41,6 +38,27 @@ angular.module('angularmeetupteachrApp')
         $http.delete('/students', {params: params}).success(function(data) {
             var index = $scope.students.indexOf(student);
             $scope.students.splice(index, 1);
+        });
+    }
+
+
+    /*****************/
+    /* LOGIN */
+    $scope.login = function(username, password) {
+        var params = {
+            username: username,
+            password : password
+        }
+        $http.post('/users/login', params).success(function(data) {
+            $scope.currentUser = {
+                username: username,
+                isAuthenticated: true,
+                sessionId: data.id,
+                id: data.uid
+            }
+            $location.path("/");
+        }).error(function(data) {
+            $log.log("Login failed: ", data);
         });
     }
     
